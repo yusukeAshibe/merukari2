c4d7c81f53b2ae486baccb9717120130c911ce92
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-05-21 20:29:18 +0900

    アイテムサービスの作成

---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+
+@Service
+@Transactional
+public class ItemService {
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	public List<Item>showItem(){
+		List<Item>itemList = itemRepository.findAll();
+		return itemList;
+		
+	}
+
+}
